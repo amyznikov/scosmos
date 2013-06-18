799137faa11832f7d8b95806c2c761bc63094a6c
Andrey Myznikov <andrey.myznikov@gmail.com>
2013-06-19 02:40:32 +0300

    1.0.9 Update matlab code formatting

--- lib/octave/prepare_reference_stars.m
+++ lib/octave/prepare_reference_stars.m
@@ -14,10 +14,10 @@ function prepare_reference_stars( plateid, refnames )
 
   sid = ssa_surveyid(plateid);
   [A0, D0, EPOCH] = ssa_plate_info( plateid );
-  
+
   for i = 1:size(refnames,2)
-  
-    refname = refnames{i}; 
+
+    refname = refnames{i};
 
     dirname = sprintf('refs/%d/%s', sid, refname );
     outname = sprintf('%s/%d.dat', dirname, plateid );
@@ -28,39 +28,39 @@ function prepare_reference_stars( plateid, refnames )
     
     syscall('mkdir -p %s', dirname );
     
-	  if ( strcmp(refname,'ucac4') )
-	    selectCmd = generate_select_ucac4( A0, D0, EPOCH);
-	  else
-	    fprintf(stderr,'prepare_reference_stars() not implemented for "%s"\n', refname);
+    if ( strcmp(refname,'ucac4') )
+      selectCmd = generate_select_ucac4( A0, D0, EPOCH);
+    else
+      fprintf(stderr,'prepare_reference_stars() not implemented for "%s"\n', refname);
       continue;
-	  end
+    end
 
-	  cmd = get_plate_pipe_command( plateid, "-hf" );
-	  if ( strcmp(cmd, '' ) )
-	    fprintf(stderr,'get_plate_pipe_command() fails for plateid=%d\n', plateid);
+    cmd = get_plate_pipe_command( plateid, "-hf" );
+    if ( strcmp(cmd, '' ) )
+      fprintf(stderr,'get_plate_pipe_command() fails for plateid=%d\n', plateid);
       return;
-	  end
+    end
 
-	  cmd = sprintf('%s | cut -f 3,4,13,14,21,33-36 > %s', cmd, stmp);
-	  disp(cmd);
-	  syscall(cmd); 
+    cmd = sprintf('%s | cut -f 3,4,13,14,21,33-36 > %s', cmd, stmp);
+    disp(cmd);
+    syscall(cmd); 
 
-	  cmd = sprintf('psql wsdb -c \\\\"copy(%s) to stdout with csv header delimiter E''\\t'' null ''NA'' \\\\" > %s', selectCmd, rtmp);
-	  disp(cmd);
-	  syscall(cmd);
+    cmd = sprintf('psql wsdb -c \\\\"copy(%s) to stdout with csv header delimiter E''\\t'' null ''NA'' \\\\" > %s', selectCmd, rtmp);
+    disp(cmd);
+    syscall(cmd);
 
-	  cmd = sprintf('ssa-pair-stars -vd %s %s rc1=1 dc1=2 rc2=1 dc2=2 r=2.5 dups=drop > %s', rtmp, stmp, ptmp);
-	  disp(cmd);
-	  syscall(cmd); 
+    cmd = sprintf('ssa-pair-stars -vd %s %s rc1=1 dc1=2 rc2=1 dc2=2 r=2.5 dups=drop > %s', rtmp, stmp, ptmp);
+    disp(cmd);
+    syscall(cmd); 
 
-	  if ( strcmp(refname,'ucac4') )
-	    cmd = sprintf('awk ''{if (FNR==1 || (\\\\$3==0 && \\\\$4==0)) { print \\\\$0; } }'' %s | cut --complement -f 3,4,5,6 > %s', ptmp, outname);
-	    disp(cmd);
-	    syscall(cmd);
-	  end
-    
+    if ( strcmp(refname,'ucac4') )
+      cmd = sprintf('awk ''{if (FNR==1 || (\\\\$3==0 && \\\\$4==0)) { print \\\\$0; } }'' %s | cut --complement -f 3,4,5,6 > %s', ptmp, outname);
+      disp(cmd);
+      syscall(cmd);
+    end
+
   end
-  
+
 end
 
 
