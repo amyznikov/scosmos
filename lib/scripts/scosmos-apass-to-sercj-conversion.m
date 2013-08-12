66c00b9648488e55b4b855faff9f532f8cf542d1
Andrey Myznikov <andrey.myznikov@gmail.com>
2013-08-12 13:04:34 +0300

    1.0.50 Add scosmos-apass-to-sercj-conversion.m

--- lib/scripts/scosmos-apass-to-sercj-conversion.m
+++ lib/scripts/scosmos-apass-to-sercj-conversion.m
@@ -1,5 +1,20 @@
 #! /usr/bin/octave -qf
 
+%
+% Перевод Bj,Vj в фильтр 'Bc' используя пересечение serc-j/apass
+% в линейной части характеристической кривой:
+%   Bc = a0 + a1*Bj + a2*(Bj-Vj) = b0 + b1 * scosmos ;
+% 
+% Система переопределена; произвол снимается избавившись
+% от свободных нуль-пункта и множителя и минимизируя дисперсию за
+% цвет:
+%   Bj = c0 + c1 * scosmos + c2*(Bj-Vj)
+%
+% Решение системы выполняется по звездам apass.
+%
+%
+
+
 
 pkg load optim;
 
@@ -7,13 +22,12 @@ arg_list = argv ();
 
 FLIST = {};
 outname = '';
-color=''
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 function show_usage( stream )
   fprintf(stream, 'USAGE:\n');
-  fprintf(stream, '  %s [-o {output-name}] -c {V|B} apass.tyc2.dat ...\n', program_name());
+  fprintf(stream, '  %s [-o {output-name}] apass.sercj.dat ...\n', program_name());
 end
 
 
@@ -33,15 +47,6 @@ while ( ++i <= nargin )
     end
     outname = arg_list{i}
 
-  elseif ( strcmp(arg_list{i},'-c') )
-
-    if ( ++i > nargin )
-      fprintf(stderr,'missing color name after -c argument\n');
-      show_usage(stdout);
-      exit(0);
-    end
-    color = arg_list{i}
-
   else
     FLIST{size(FLIST,2)+1} = arg_list{i};
   end
@@ -54,17 +59,6 @@ if ( size(FLIST,2) < 1 )
   exit(1);
 end
 
-if ( strcmp(color,'' ) )
-  fprintf(stderr,'Target color not specified\n');
-  show_usage(stderr);
-  exit(1);
-end
-
-if ( ~strcmp(color,'B') && ~strcmp(color,'V') )
-  fprintf(stderr,'Invalid target color specified\n');
-  show_usage(stderr);
-  exit(1);
-end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -86,22 +80,18 @@ for i = 1:size(FLIST,2)
 
 
 
-  % load BT,VT,Bj
-
+  % load Bj,Vj,scosmos
+ 
   printf('\n');
   printf('%s\n',fname);
 
+  columns = 'Johnson_B,Johnson_V,cosmag,isky,ra,dec,x,y';
 
-  switch color
-    case 'B'  columns = 'btmag,vtmag,Johnson_B,radeg,decdeg';
-    case 'V'  columns = 'btmag,vtmag,Johnson_V,radeg,decdeg';
-  end
-
-  cond = [ '@e_btmag<0.5 && @e_vtmag<0.5 && ' ...
-           '(@btmag-@vtmag)>-0.5 && (@btmag-@vtmag)<3 && ' ...
-           '@Johnson_B!=\"NA\" && @Johnson_V!=\"NA\" && ' ...
+  cond = [ '@Johnson_B!=\"NA\" && @Johnson_V!=\"NA\" && ' ...
            '(@Johnson_B-@Johnson_V)>-0.5 && (@Johnson_B-@Johnson_V)<2.5 && ' ...
-           '@Johnson_B>10 && @Johnson_V>9.5' ...
+           '@Johnson_B>10 && @Johnson_V>9.5 &&' ...
+           '@cosmag>-26 && @cosmag<-23 && '  ...
+	   'abs(@prfStat) < 4'
           ];
 
   cmd = sprintf('ccut %s -f "%s" -x "%s"', fname, columns, cond);
@@ -110,52 +100,62 @@ for i = 1:size(FLIST,2)
   if ( pid == -1 ||  ~ischar(fgets(pid)) )
     fprintf(stderr,'can not read %s\n', fname);
   else
-    v = transpose( fscanf(pid,'%lf',[5,Inf]) );
+    v = transpose( fscanf(pid,'%lf',[8,Inf]) );
   end
   pclose(pid);
 
-  if ( size(v,1) < 3 || size(v,2) != 5 )
+  if ( size(v,1) < 3 || size(v,2) != 8 )
     fprintf(stderr,'empty dataset %s\n', fname);
     continue;
   end
 
 
   % extract columns
-  BT = v(:,1);
-  VT = v(:,2);
-  MJ = v(:,3);
+  Bj     = v(:,1);
+  Vj     = v(:,2);
+  cosmag = v(:,3);
+  isky   = v(:,4);
+  ra     = v(:,5);
+  dec    = v(:,6);
+  x      = v(:,7); 
+  y      = v(:,8); 
+  RA     = mean(ra)*180/pi;
+  DE     = mean(dec)*180/pi;
 
-  RA = mean(v(:,4));
-  DE = mean(v(:,5));
-
-  % make linear reduction 
-  %  BJ = A0 + A1*BT + A2*(BT-VT)
-  %  VJ = A0 + A1*VT + A2*(BT-VT)
-
+  % Make linear regression
+  %  Bj = c0 + c1 * scosmos + c2*(Bj-Vj)
+  %
+  % The Bc will defined as:
+  %  Bc = c0 + c1 * scosmos
 
   K = 3;
-  NPASS = 5;
+  NPASS = 1;
 
   for pass = 1:NPASS
 
-    switch color
-      case 'B'  MM = [ ones(size(MJ)) BT (BT-VT) ];
-      case 'V'  MM = [ ones(size(MJ)) VT (BT-VT) ];
-    end
+    MM = [ ones(size(cosmag)) cosmag (Bj-Vj) ];
 
-    [A, sigma] = LinearRegression(MM, MJ);
+    [A, sigma] = LinearRegression(MM, Bj);
     sigma = sqrt(sigma);
 
     printf('* %d sigma=%g rows=%d\n', pass, sigma, rows(MM));
-    disp(A);
+    printf('A1 = %+g\n',A(1));
+    printf('A2 = %+g\n',A(2));
+    printf('A3 = %+g\n',A(3));
+   
 
-    if ( K == 0 || pass == NPASS || all(good=abs(MJ-MM*A)<K*sigma) )
+    if ( K == 0 || pass == NPASS || all(good=abs(Bj-MM*A)<K*sigma) )
       break;
     end
 
-    BT = BT(good);
-    VT = VT(good);
-    MJ = MJ(good);
+    Bj = Bj(good);
+    Vj = Vj(good);
+    cosmag = cosmag(good);
+    isky = isky(good);
+    ra = ra(good);
+    dec = dec(good);
+    x = x(good);
+    y = y(good);
 
   end
 
@@ -183,6 +183,19 @@ for i = 1:size(FLIST,2)
       fname, RA, DE, rows(MJ), sigma, A(1), A(2), A(3) );
    fclose(fid);
   end
+
+  fname = sprintf('%s.res',fname);
+  fid = fopen(fname,'w');
+  fprintf(fid,'ra\tdec\tx\ty\tr\tBj\tVj\tcosmag\tisky\tBc\tdM\n');
+  
+  Bc = A(1) + A(2) * cosmag;
+  dM = Bj - A(3)*(Bj-Vj) - Bc;
+  r  = sqrt((x - 177500).^2 + (y - 177500).^2); 
+
+  fprintf(fid,'%.9f\t%+.9f\t%.3f\t%.3f\t%.3f\t%.3f\t%.3f\t%.3f\t%.3f\t%.3f\t%+.3f\n',
+   transpose([ra*180/pi dec*180/pi x y r Bj Vj cosmag isky Bc dM]));
+
+  fclose (fid);
 
 end
 
